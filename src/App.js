d85ed799d91867e22be9682db106ca91fdf91900
Ilyas Garipov <ilyasgaripov97@gmail.com>
2021-02-19 17:13:59 +0300

    Refactor Card component, pass child elements to it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 import Card from './components/Card';
+import Header from './components/Header'
+import InputField from './components/InputField';
+import Logo from './components/Logo';
+import Button from './components/Button';
 
 
 function App() {
   return (
-    <div className="App container">
-      <Card titleText={"Добро пожаловать!"} withLogo={true}/>
-    </div>
+    <Router>
+      {/* <nav>
+        <ul>
+          <Link to="/">Home</Link>
+        </ul>
+        <ul>
+          <Link to="/other">Other</Link>
+        </ul>
+      </nav> */}
+      <Switch>
+        <Route path="/restore" exact>
+          <div className="App container">
+            <Card>
+              <Logo />
+              <Header titleText={"Восстрановить пароль"} />
+              <InputField label={"Электронная почта"} type={"email"} placeholder="yammy@gmail.com" />
+              <Button text={"Восстановить"} isUpper={true} isPrimary={true} hasMargin={true} />
+            </Card>
+          </div>
+        </Route>
+        <Route path="/">
+          <div className="App container">
+            <Card>
+              <Logo />
+              <Header titleText={"Добро пожаловать"} />
+              <InputField label={"Электронная почта"} type={"email"} placeholder="yammy@gmail.com" />
+              <InputField label={"Пароль"} type={"password"} placeholder="yoursecret" />
+              <Button text={"Войти"} isUpper={true} isPrimary={true} hasMargin={true} />
+              <Button text={"Создать аккаунт"} isUpper={true} isAccent={true} hasMargin={true} />
+            </Card>
+          </div>
+        </Route>
+        
+      </Switch>
+    </Router>
+
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,16 @@
 import './Card.css'
 
-import Header from './Header'
-import InputField from './InputField';
-import Logo from './Logo';
-import Button from './Button';
+import { useState } from 'react'
 
 
-const Card = ({titleText, withLogo}) => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="card__content">
-        {withLogo && <Logo/>}
-        <Header titleText={titleText}/>
-        <InputField label={"Электронная почта"} type={"email"} placeholder="yammy@gmail.com" />
-        <InputField label={"Пароль"} type={"password"} />
-        <Button text={"Войти"} isUpper={true} isPrimary={true} hasMargin={true}/>
-        <Button text={"Создать аккаунт"} isUpper={true} isAccent={true} hasMargin={true}/>
+        {props.children}
       </div>
-
     </div>
   )
-}
-
-Card.defaultProps = {
-  withLogo: false,
 }
 
 export default Card
