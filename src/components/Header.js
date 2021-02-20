17a961b146f09e7cdc1f98b6d78f6feb8c1fbfaf
Ilyas Garipov <ilyasgaripov97@gmail.com>
2021-02-21 01:24:15 +0300

    Add default margin for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import './Header.css'
 
-const Header = ({ titleText }) => {
+
+const Header = ({ titleText, hasMargin=true }) => {
+
+  let className = "header";
+
+  className += hasMargin ? " header__m-default" : "";
+  console.log(className);
+  
   return (
-    <div className="header">
+    <div className={className}>
       <h1 className="header__title">{titleText}</h1>
     </div>
   )
