b6efaefcb82a822c8bcc8ca26627dd2f5d8e5784
Ilyas Garipov <ilyasgaripov97@gmail.com>
2021-02-23 11:14:14 +0300

    Access api with examples, post/get request

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,14 +4,39 @@ import Header from './Header';
 import InputField from './InputField';
 import Button from './Button';
 
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 const Welcome = ({ handleOnClick }) => {
+  const csrftoken = getCookie('csrftoken');
+  let [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    fetch("http://..")
+
+    fetch("http://127.0.0.1:8000/api/examples/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+    })
       .then(res => res.json())
       .then(
         (result) => {
@@ -21,11 +46,33 @@ const Welcome = ({ handleOnClick }) => {
           console.log(error);
         }
       )
-  }, [])
+  
 
+    // fetch("http://127.0.0.1:8000/api/examples/", {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'X-CSRFToken': csrftoken,
+    //   },
+    //   body: JSON.stringify({ name: 'qqq'})
+    // })
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       console.log(result.Error);
+    //       if (result.Error === null) setLoggedIn(true); 
+    //     },
+    //     (error) => {
+    //       console.log(error);
+    //     }
+    //   )
+
+
+  }, [])
   return (
     <div>
       <Card>
+        {loggedIn ? <Redirect to="/home" /> : ""}
         <Logo />
         <Header titleText={"Добро пожаловать"} />
         <InputField label={"Электронная почта"} type={"email"} placeholder="yammy@gmail.com" />
