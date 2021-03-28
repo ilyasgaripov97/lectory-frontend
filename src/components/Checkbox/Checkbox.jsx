11023af856efa80400dbb8faceb62d3f4a513197
ilyasgaripov97 <ilyasgaripov97@gmail.com>
2021-03-28 23:52:05 +0300

    Add Checkbox component

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,10 +3,6 @@ import './Checkbox.css'
 
 const Checkbox = ({ handleClick, checked }) => {
 
-  const logger = () => {
-    console.log('Logger call: ', checked);
-  }
-
   return (
     <div className="checkbox-wrapper" >
       <input type="checkbox" defaultChecked={checked} onClick={handleClick}/>
