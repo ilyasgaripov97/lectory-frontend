1c9738bb445fd908ccbf2fca1b00fd8b0df627a5
ilyasgaripov97 <ilyasgaripov97@gmail.com>
2021-04-16 00:10:21 +0300

    Searchbox refresh page on clear

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,6 +9,10 @@ const Sidebar = ({ materials, setMaterials }) => {
   const like = (expect, actual) => actual.includes(expect)
 
   const filterMaterials = (e) => {
+    if (e.target.value === '') {
+      window.location.reload();
+      console.log('empty searchbox');
+    }
     const updatedMaterials = materials.filter(material => like(e.target.value, material.title));
     setMaterials(updatedMaterials);
   }
